e4ffef980f9207c65e4de2bec5f0d95aef743146
Vitaliy Murzin <murzinvit@yandex.ru>
2024-12-20 18:55:55 +0500

    init commit

---
+++ src/test/java/ru/netology/file_manager/FileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.file_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
