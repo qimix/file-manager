7006282fb201bac26936c2b0513ae3f3dce4b630
Vitaliy Murzin <murzinvit@yandex.ru>
2024-12-22 02:33:00 +0500

    add logout

--- src/main/java/ru/netology/file_manager/controller/AuthController.java
+++ src/main/java/ru/netology/file_manager/controller/AuthController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +27,12 @@ public class AuthController {
     @PostMapping("/login")
     public JwtAuthenticationResponse login(@RequestBody @Valid SignInFrontendRequest frontendRequest) {
         return authenticationService.login(frontendRequest);
+    }
+
+    @Operation(summary = "User logout")
+    @PostMapping("/logout")
+    public ResponseEntity<String> login() {
+        return new ResponseEntity<String>("Logout user", HttpStatus.OK);
     }
 
     @Operation(summary = "Регистрация пользователя")
