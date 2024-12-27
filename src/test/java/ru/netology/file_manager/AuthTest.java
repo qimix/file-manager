a375091a3087c3dbd632e433d05ca39e9e8ae4ee
Vitaliy Murzin <murzinvit@yandex.ru>
2024-12-28 01:28:31 +0500

    add authtest

---
+++ src/test/java/ru/netology/file_manager/AuthTest.java
@@ -0,0 +1,42 @@
+package ru.netology.file_manager;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import ru.netology.file_manager.controller.AuthController;
+import ru.netology.file_manager.dto.JwtAuthenticationResponse;
+import ru.netology.file_manager.dto.SignInFrontendRequest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest
+public class AuthTest {
+    private SignInFrontendRequest frontendRequest;
+    @Autowired
+    private AuthController authController;
+
+    @BeforeEach
+    public void initData() {
+        frontendRequest = mock(SignInFrontendRequest.class);
+        Mockito.when(frontendRequest.getEmail()).thenReturn("admin@gmail.com");
+        Mockito.when(frontendRequest.getPassword()).thenReturn("$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha");
+    }
+
+    @Test
+    @DisplayName("JUnit test for AuthController.login")
+    void testAuthController() {
+        JwtAuthenticationResponse authenticationResponse = authController.login(frontendRequest);
+        assertThat(authenticationResponse).isNotNull();
+    }
+
+
+}
