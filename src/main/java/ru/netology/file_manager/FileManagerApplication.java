0bcf51354e51a90d6a8b19d91fdb1416703f5a9f
Vitaliy Murzin <murzinvit@yandex.ru>
2025-01-02 05:00:52 +0500

    add logger

--- src/main/java/ru/netology/file_manager/FileManagerApplication.java
+++ src/main/java/ru/netology/file_manager/FileManagerApplication.java
@@ -1,10 +1,14 @@
 package ru.netology.file_manager;
 
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FileManagerApplication {
+	static final Logger log =
+            (Logger) LoggerFactory.getLogger(FileManagerApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(FileManagerApplication.class, args);
 	}
